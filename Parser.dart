dc1a7877405cb5fc380280a5d308b29404c41911
Andrea Boriero <dreborier@gmail.com>
2012-05-23 08:54:41 +0100

    refactored code, more refactoring needed :-P

--- Parser.dart
+++ Parser.dart
@@ -1,20 +1,21 @@
 class Parser {
   
-  String baseUrl = "http://api.dartlang.org/";
-  
-  String json;
-  
-  Parser(){
+  final String baseUrl = "http://api.dartlang.org/";
+  String _json;
+  Request _request;
     
+  Parser(){
   }
   
-  Future<List<String>> getUrlsStartingWith(String name){
-    Request req = new Request();
+  set reaquest(Request request) => this._request = request;
+  
+  Future<List<String>> getUrlsStartingWith(String searchedString){
     Completer resultComplete = new Completer();
-    Future<String> future = req.makeRequest();
-    future.then((jsonfuture){
-      json = jsonfuture;
-      List<Result> r = _parseStartWith(name);
+    Future<String> future = _request.makeRequest();
+    
+    future.then((jsonFuture){
+      _json = jsonFuture;
+      List<Result> r = _parseStartWith(searchedString);
       
       List<String> url = new List();
       
@@ -23,26 +24,29 @@ class Parser {
       });
       resultComplete.complete(url);
     });
+    
     return resultComplete.future;
   }
    
   List<Result> _parseStartWith(String name){
-    Map<String, Object> parsedJson = JSON.parse(json);
+    Map<String, Object> parsedJson = JSON.parse(_json);
     List<Result> results = new List<Result>();
     
     var packagesKeys = parsedJson.getKeys();
+    
     packagesKeys.forEach((k){
       
       var packageValues = parsedJson[k];
       
       packageValues.forEach((v){
-        
         if(v["name"].toUpperCase().startsWith(name.toUpperCase())){
           Result result = new Result(v["name"],v["kind"],baseUrl + v["url"]);
           results.add(result);
         }
       });
+      
     });
+    
     return results;
   }
 }
--- dartExtension.dart
+++ dartExtension.dart
@@ -9,6 +9,7 @@
 
 callFromJavascript(String name, var suggestFn, var f){
   List results = new List();
+  
   if(name.toLowerCase() == "home"){
     results.add("darlang.org , http://www.dartlang.org/");
     f(results,suggestFn,name) ;
@@ -17,36 +18,22 @@ callFromJavascript(String name, var suggestFn, var f){
     results.add("language specification , http://www.dartlang.org/docs/spec/latest/dart-language-specification.html");
     f(results,suggestFn,name);
   }else{
-  
-  Parser p = new Parser();
-  Future<List<String>> future = p.getUrlsStartingWith(name);
-  future.then((resultFuture){
-    results = resultFuture;
-        if(results.length == 0 && name.length > 2){
-          results.add("dartlang.org : , http://www.dartlang.org/search.html?&q=${name}");
-        }
-        f(results,suggestFn,name);
-  });
-  
+    Parser p = new Parser();
+    p.reaquest = new Request();
+    
+    Future<List<String>> future = p.getUrlsStartingWith(name);
+    
+    future.then((resultFuture){
+      results = resultFuture;
+          if(results.length == 0 && name.length > 2){
+            results.add("dartlang.org : , http://www.dartlang.org/search.html?&q=${name}");
+          }
+          f(results,suggestFn,name);
+    });
   }
   
 }
 
-dartCallback(String data) {
-  print(data);
-  var obj = JSON.parse(data);
-//  print(obj);
-//  Element div = document.query("#jsondiv");
-}
-
 void main() {
-  
-  //var f = dartCallback;
   var t = callFromJavascript;
-  
-  //var f = dartCallback;
-  //Element script = new Element.tag("script");
-  //script.src = "http://api.dartlang.org/nav.json?callback=callbackForJsonpApi";
-//script.src = "https://ajax.googleapis.com/ajax/services/search/news?v=1.0&q=barack%20obama&callback=callbackForJsonpApi";
-  //document.body.elements.add(script);
 }
